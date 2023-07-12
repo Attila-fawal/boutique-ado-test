1df75d2b142194d0a2c6b5b2294408f2959dbec2
Attila fawal <attila_faweli@hotmail.com>
2023-07-12 07:33:08 +0000

    added products app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-attilafawal-boutiqueado-dmgcuesstz3.ws-us101.gitpod.io']
+ALLOWED_HOSTS = ['8000-attilafawal-boutiqueado-foce5t68kbh.ws-us101.gitpod.io']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
